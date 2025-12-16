b08f46bd63dd50da0eb545f50b6842eebec486ac
Jephte Clain <Jephte.Clain@univ-reunion.fr>
2025-12-16 10:44:03 +0400

    modifs.mineures sans commentaires

--- tbin/sync.sh
+++ tbin/sync.sh
@@ -2,9 +2,13 @@
 # -*- coding: utf-8 mode: sh -*- vim:sw=4:sts=4:et:ai:si:sta:fenc=utf-8
 source /etc/nulib.sh || exit 1
 
-SOURCE=root@pegase-dre2023.univ.run:/data/dremgr
+prod_SOURCE=root@pegase-dre2023.univ.run:/data/dremgr
+test_SOURCE="$prod_SOURCE"
+demo_SOURCE=root@sda-demo2025.univ.run:dremgr/var
 
 profile=
+source_profile=
+dest_profile=
 date=
 sync=
 import=
@@ -14,6 +18,9 @@ args=(
     -g:,--profile profile=
     -P,--prod profile=prod
     -T,--test profile=test
+    -M,--demo profile=demo
+    --sg:,--source-profile source_profile=
+    --dg:,--dest-profile dest_profile=
     -@:,--date date=
     -u,--sync sync=1
     -i,--import import=1
@@ -29,11 +36,15 @@ cd "$MYDIR/.."
 
 if [ -n "$sync" ]; then
     [ -n "$profile" ] || profile=prod
+    [ -n "$source_profile" ] || source_profile="$profile"
+    [ -n "$dest_profile" ] || dest_profile="$source_profile"
+    SOURCE="${source_profile}_SOURCE"; SOURCE="${!SOURCE}"
     args=(
         -vrltp
-        --include "*$date*" --exclude "*" --delete-excluded
-        "$SOURCE/${profile}-dredata/downloads/"
-        "var/${profile}-dredata/downloads/"
+        --include "*$date*" --exclude "*"
+        --delete-excluded --delete-after
+        "$SOURCE/${source_profile}-dredata/downloads/"
+        "var/${dest_profile}-dredata/downloads/"
     )
     rsync "${args[@]}"
 fi
