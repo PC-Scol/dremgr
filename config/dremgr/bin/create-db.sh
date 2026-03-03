0df3f3747eebc149fe2e12fa291751c13d3f42b1
Jephte Clain <Jephte.Clain@univ-reunion.fr>
2026-03-03 12:25:42 +0400

    remanier et corriger la gestion des droits

--- config/dremgr/bin/create-db.sh
+++ config/dremgr/bin/create-db.sh
@@ -1,11 +1,26 @@
 #!/bin/bash
 # -*- coding: utf-8 mode: sh -*- vim:sw=4:sts=4:et:ai:si:sta:fenc=utf-8
 
+create_db=1
+defaults=
+revoke=
+args="$(getopt -o ndK -l no-create,defaults,revoke -n "$0" -- "$@")" || exit 1
+eval "set -- $args"
+while [ $# -gt 0 ]; do
+    arg="$1"; shift
+    case "$arg" in
+    --) break;;
+    -n|--no-create) create_db=;;
+    -d|--defaults) defaults=1;;
+    -K|--revoke) revoke=1;;
+    esac
+done
+
 unset PGDATABASE
 psql <<EOF
-create database $DBNAME;
+${create_db:+create database $DBNAME;}
 
 \connect $DBNAME
 
-$(pg_grant_default_privileges)
+$(pg_grant_privileges -r ${defaults:+-d} ${revoke:+--revoke})
 EOF
--- config/dremgr/bin/create-pdb.sh
+++ config/dremgr/bin/create-pdb.sh
@@ -1,13 +1,28 @@
 #!/bin/bash
 # -*- coding: utf-8 mode: sh -*- vim:sw=4:sts=4:et:ai:si:sta:fenc=utf-8
 
+create_db=1
+defaults=
+revoke=
+args="$(getopt -o ndK -l no-create,defaults,revoke -n "$0" -- "$@")" || exit 1
+eval "set -- $args"
+while [ $# -gt 0 ]; do
+    arg="$1"; shift
+    case "$arg" in
+    --) break;;
+    -n|--no-create) create_db=;;
+    -d|--defaults) defaults=1;;
+    -K|--revoke) revoke=1;;
+    esac
+done
+
 if [ -n "$PDBNAME" ]; then
     unset PGDATABASE
     psql <<EOF
-create database $PDBNAME;
+${create_db:+create database $PDBNAME;}
 
 \connect $PDBNAME
 
-$(pg_grant_default_privileges)
+$(pg_grant_privileges ${defaults:+-d} ${revoke:+--revoke})
 EOF
 fi
